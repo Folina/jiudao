344113a0737a9f0e51b4d49aeec5f132e9533861
xiaocc <cc13538560247@gmail.com>
2019-08-13 15:03:44 +0800

    用三元表达式的方式来改变图片的状态

--- components/like/index.js
+++ components/like/index.js
@@ -11,13 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    //数据绑定
+    //三元表达式
+    like:true,
+    count1:99,
+    count2:999,
+    yesSrc:"images/like.png",
+    noSrc:"images/like@dis.png"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike:function(event){
+      console.log("1111")
+    }
   }
 })
