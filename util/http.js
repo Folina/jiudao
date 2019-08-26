527b7da9d93909ee13bf43f4fe7288abc7c760c9
Folina <cc13538560247@gmail.com>
2019-08-26 23:44:55 +0800

    错误处理

--- util/http.js
+++ util/http.js
@@ -1,5 +1,10 @@
 import{config} from '../config.js'
-
+const tips ={
+  "1":"出现一个错误",
+  "1007":"没有开发者key",
+  "3000":"没有刊期"
+  
+}
 class HTTP{
   request(params){
     if(!params.method){
@@ -15,18 +20,33 @@ class HTTP{
       },
       success:(res) =>{
         //startsWith、endsWith
-        let code = res.statusCode
+        let code = res.statusCode.toString()
         if(code.startsWith('2')){
+          params.success(res.data)
 
         }
         else{
+         let error_code = res.data.error_code
+         this._show_error(error_code)
 
         }
 
       },
       fail:(err) =>{
+        this._show_error(1)
 
       }
+    })
+
+  }
+  _show_error(error_code){
+    if(!error_code){
+        error_code=1
+    }
+    wx.showToast({
+      title:tips[error_code],
+      icon:"none",
+      duration:2000
     })
 
   }
